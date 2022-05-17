02f52c24c1777b1ca23c6dd3a76eba27eb147d7a
Justin Schroeder <justin@formkit.com>
2022-05-17 12:09:08 -0400

    Adds cards example

--- docs/src/sections/SectionExamples.vue
+++ docs/src/sections/SectionExamples.vue
@@ -1,11 +1,17 @@
 <script setup>
 import ActualCards from "../examples/cards/ActualCards.vue"
+import IconOutbound from "../components/IconOutbound.vue"
 </script>
 
 <template>
   <section id="examples">
     <h2>Examples</h2>
-    <h3>Adding cards</h3>
+    <h3 id="example-cards">
+      <a
+        href="https://github.com/formkit/auto-animate/blob/master/docs/src/examples/cards/ActualCards.vue"
+        >Adding cards <IconOutbound
+      /></a>
+    </h3>
     <ActualCards />
   </section>
 </template>
