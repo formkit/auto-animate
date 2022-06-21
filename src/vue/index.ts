8f00f4c99c1a43c4f1d335d52308d84270720440
AlexVipond <alex.r.vipond@gmail.com>
2022-06-20 20:31:32 -0500

    adds useAutoAnimate vue composable

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -1,8 +1,30 @@
-import type { Plugin } from "vue"
-import { vAutoAnimate } from "../index"
+import { ref, onMounted, Plugin, Ref } from "vue"
+import autoAnimate, { vAutoAnimate, AutoAnimateOptions, AutoAnimationPlugin } from "../index"
 
 export const autoAnimatePlugin: Plugin = {
   install(app) {
     app.directive("auto-animate", vAutoAnimate)
   },
+}
+
+/**
+ * AutoAnimate hook for adding dead-simple transitions and animations to Vue.
+ * @param options - Auto animate options or a plugin
+ * @returns A function ref, which you should bind to the `ref` attribute
+ * of your target element so `autoAnimate` can access it.
+ */
+ export function useAutoAnimate<T extends Element>(
+  options: Partial<AutoAnimateOptions> | AutoAnimationPlugin = {}
+) {
+  const element = ref<T>()
+  const functionRef: (el: T) => void = el => {
+    if (el) element.value = el
+  }
+
+  onMounted(() => {
+    if (element.value instanceof HTMLElement)
+      autoAnimate(element.value, options)
+  })
+
+  return functionRef
 }
