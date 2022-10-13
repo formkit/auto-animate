010498438de4fe4259122b8cbf4bedc06da73992
Łukasz Szarpak <61237195+lukaszarpak@users.noreply.github.com>
2022-10-13 10:28:19 +0200

    Update component's order

--- docs/src/sections/SectionUsage.vue
+++ docs/src/sections/SectionUsage.vue
@@ -146,12 +146,11 @@ import IconSvelte from "../components/IconSvelte.vue"
     <CodeExample :examples="vueDirectiveApp" title="App" />
     <ActualVueApp />
     <AsideTip>
+      Please bare in mind that value passed to <code>:key</code> should be a unique value, otherwise animation might not work as expected.
+    </AsideTip>
+    <AsideTip>
       Vue users can pass options by directly setting the directive’s value
       <code>&lt;ul v-auto-animate="{ duration: 100 }"&gt;</code>
-    </AsideTip>
-    
-    <AsideTip>
-      Please bare in mind that value passed to <code>:key</code> should be an unique value, otherwise animation might not work as expected.
     </AsideTip>
 
     <h3 id="usage-vue-composable">Vue composable</h3>
