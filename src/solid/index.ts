5e52a6ecbd74a4188a974e138faa65f5eab26fb1
Nir Tamir <nirtamir2@gmail.com>
2023-01-22 23:12:20 +0200

    use @ardeora code for auto-animate

--- src/solid/index.ts
+++ src/solid/index.ts
@@ -1,15 +1,11 @@
-import { createEffect } from "solid-js"
-import autoAnimateBase, {
+import autoAnimate, {
   AutoAnimateOptions,
   AutoAnimationPlugin,
+  AnimationController,
 } from "../index"
-
-/**
- * This code is taken from https://github.com/lxsmnsyc/solid-auto-animate/blob/main/packages/solid-auto-animate/src/index.ts
- */
+import { createSignal, onMount, Setter, Accessor } from "solid-js"
 
 declare module "solid-js" {
-  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
     interface Directives {
       autoAnimate: Partial<AutoAnimateOptions> | AutoAnimationPlugin | true
@@ -17,21 +13,42 @@ declare module "solid-js" {
   }
 }
 
-export function autoAnimate<T extends HTMLElement>(
-  element: T,
-  options: () => Partial<AutoAnimateOptions> | AutoAnimationPlugin | true
-): void {
-  createEffect(() => {
-    const currentOptions = options()
-    autoAnimateBase(element, currentOptions === true ? {} : currentOptions)
+export const createAutoAnimate = <T extends HTMLElement>(
+  options: Partial<AutoAnimateOptions> | AutoAnimationPlugin = {}
+): [Setter<T | null>, (enabled: boolean) => void] => {
+  const [element, setElement] = createSignal<T | null>(null)
+
+  let controller: AnimationController | undefined
+  // Will help us set enabled even before the element is mounted
+  let active = true
+
+  onMount(() => {
+    const el = element()
+    if (el) {
+      controller = autoAnimate(el, options)
+      if (active) controller.enable()
+      else controller.disable()
+    }
   })
+
+  const setEnabled = (enabled: boolean) => {
+    active = enabled
+    if (controller) {
+      enabled ? controller.enable() : controller.disable()
+    }
+  }
+
+  return [setElement, setEnabled]
 }
 
-export function createAutoAnimate<T extends HTMLElement>(
-  element: () => T,
-  options: Partial<AutoAnimateOptions> | AutoAnimationPlugin
-) {
-  createEffect(() => {
-    autoAnimateBase(element(), options)
-  })
+export const createAutoAnimateDirective = () => {
+  return (
+    el: HTMLElement,
+    options: Accessor<Partial<AutoAnimateOptions> | AutoAnimationPlugin | true>
+  ) => {
+    let optionsValue = options()
+    let resolvedOptions: Partial<AutoAnimateOptions> | AutoAnimationPlugin = {}
+    if (optionsValue !== true) resolvedOptions = optionsValue
+    autoAnimate(el, resolvedOptions)
+  }
 }
