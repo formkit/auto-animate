5ed1a3aff5d3577312b6a093433afa3b231cb39c
banqinghe <qingheban@qq.com>
2023-03-08 00:37:13 +0800

    Add disable button to react example

--- docs/src/examples/react/ActualReactApp.vue
+++ docs/src/examples/react/ActualReactApp.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { useAutoAnimate } from "../../../../src/vue/index.ts"
 import { ref } from "vue"
-const [parent] = useAutoAnimate()
+const [parent, setEnabled] = useAutoAnimate()
 const items = ref([0, 1, 2])
 </script>
 
@@ -12,6 +12,9 @@ const items = ref([0, 1, 2])
     </ul>
     <button @click="items.push(items.length)" class="button button--alt">
       Add number
+    </button>
+    <button @click="setEnabled(false)" class="button button--alt">
+      Disable
     </button>
   </div>
 </template>
