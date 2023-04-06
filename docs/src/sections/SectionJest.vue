fad224d41c584362b2c91350acc056662787c6a8
Arthur Pedroti <arthurpedroti@MacBook-Air-de-Arthur.local>
2023-04-06 18:05:53 -0300

    docs: improving the introductory text on Jest section

--- docs/src/sections/SectionJest.vue
+++ docs/src/sections/SectionJest.vue
@@ -12,14 +12,14 @@ import AsideTip from "../components/AsideTip.vue"
     <p>
       Jest users may face some errors when using Auto Animate with React,
       because we are using some features which we cannot mock on our own.
-      But it's easy to solve it yourself, and we suggest you mock the whole
-      module to be sure.
+      But it's easy to solve it yourself, and we suggest you mock the
+      autoAnimate functions and the hook.
     </p>
     <p>
       To do that with Jest, you just need to create a folder that replicates
-      the module's folder structure and mock the react file:
+      the module's folder structure and mock functions:
     </p>
-    <h3>Normal approach:</h3>
+    <h3>If you are using the normal approach:</h3>
     <CodeExample title="react.js" :examples="jestReact"></CodeExample>
     <h3>Using React Hook:</h3>
     <CodeExample title="react.js" :examples="jestReactHook"></CodeExample>
