5d7f7c45960801b4bea71d49d3f7148a1e0e8f91
Justin Schroeder <justin@formkit.com>
2023-09-05 14:33:40 -0400

    Formats code to prettier spec

--- src/react/index.ts
+++ src/react/index.ts
@@ -16,13 +16,16 @@ export function useAutoAnimate<T extends Element>(
   const [controller, setController] = useState<
     AnimationController | undefined
   >()
-  const element = useCallback((node: T) => {
-    if (node instanceof HTMLElement) {
-      setController(autoAnimate(node, options))
-    } else {
-      setController(undefined)
-    }
-  }, [options])
+  const element = useCallback(
+    (node: T) => {
+      if (node instanceof HTMLElement) {
+        setController(autoAnimate(node, options))
+      } else {
+        setController(undefined)
+      }
+    },
+    [options]
+  )
   const setEnabled = useCallback(
     (enabled: boolean) => {
       if (controller) {
